f579320dcee611d71fa16f23569c5c47a8cfe295
pgorzelany <piotr.gorzelany@gmail.com>
2016-09-11 20:09:41 +0200

    Refactored side menu to use the animation delegate

--- PGSideMenu/Classes/PGSideMenu.swift
+++ PGSideMenu/Classes/PGSideMenu.swift
@@ -68,31 +68,7 @@ open class PGSideMenu: UIViewController {
         return PGSideMenuSlideInRotateAnimator(sideMenu: self)
     }()
     
-    fileprivate var maxAbsoluteContentTranslation: CGFloat {
-        return UIScreen.main.bounds.width * self.menuPercentWidth
-    }
-    
-    /** The maximum angle (in degrees) the door menu can open */
-    fileprivate let menuTilt: CGFloat = 45
-    
-    /** Content translation at the beggining of the pan gesture */
-    fileprivate var initialContentTranslation: CGFloat = 0
-    
-    fileprivate var animating = false
-    
-    fileprivate var isLeftMenuShown: Bool {
-        
-        return self.contentViewCenterConstraint.constant == self.maxAbsoluteContentTranslation
-        
-    }
-    
-    fileprivate var isRightMenuShown: Bool {
-        
-        return self.contentViewCenterConstraint.constant == -self.maxAbsoluteContentTranslation
-        
-    }
-    
-    fileprivate lazy var contentOverlayView: UIView = {
+    lazy var contentOverlayView: UIView = {
         let overlayView = UIView()
         overlayView.backgroundColor = UIColor.clear
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(overlayTouched))
@@ -163,49 +139,26 @@ open class PGSideMenu: UIViewController {
     /** Open/close left menu depending on menu state. */
     open func toggleLeftMenu() {
         
-        self.toggleMenu(.left)
+        self.animationDelegate.toggleLeftMenu(animated: true)
         
     }
     
     /** Open/close right menu depending on menu state. */
     open func toggleRightMenu() {
         
-        self.toggleMenu(.right)
+        self.self.animationDelegate.toggleRightMenu(animated: true)
     }
     
     /** Hides whatever menu is shown. */
     open func hideMenu(animated: Bool = true) {
         
-        guard self.contentViewCenterConstraint.constant != 0 else {return}
-        
-        self.contentOverlayView.removeFromSuperview()
-        
-        self.contentViewCenterConstraint.constant = 0
-        
-        if animated {
-            
-            UIView.animate(withDuration: self.menuAnimationDuration, delay: 0, options: self.menuAnimationOptions, animations: {
-                
-                self.contentContainerView.layer.transform = CATransform3DIdentity
-                self.view.layoutSubviews()
-                
-                }, completion: nil)
-            
-        } else {
-            
-            self.contentContainerView.layer.transform = CATransform3DIdentity
-            self.view.layoutSubviews()
-            
-        }
-        
+        self.animationDelegate.closeMenu(animated: animated)
     }
     
     // MARK: Private methods
     
     fileprivate func configureController(){
         
-        self.leftMenuWidthConstraint.constant = self.maxAbsoluteContentTranslation
-        self.rightMenuWidthConstraint.constant = self.maxAbsoluteContentTranslation
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognized))
         self.view.addGestureRecognizer(panGestureRecognizer)
         
@@ -222,10 +175,10 @@ open class PGSideMenu: UIViewController {
     }
     
     func overlayTouched(_ recognizer: UITapGestureRecognizer) {
-        self.hideMenu()
+        self.animationDelegate.closeMenu(animated: true)
     }
     
-    fileprivate func addContentOverlay() {
+    func addContentOverlay() {
         
         if self.contentOverlayView.superview == nil && self.hideMenuOnContentTap {
             self.contentController?.view.addSubviewFullscreen(self.contentOverlayView)
@@ -234,120 +187,7 @@ open class PGSideMenu: UIViewController {
     
     func panGestureRecognized(_ recognizer: UIPanGestureRecognizer) {
         
-        let translation = recognizer.translation(in: self.view)
-        
-        switch recognizer.state {
-            
-        case .began:
-            
-            self.initialContentTranslation = self.contentViewCenterConstraint.constant
-            
-        case .changed:
-            
-            self.translateContentView(inXDimension: translation.x + self.initialContentTranslation)
-            
-        case .ended:
-            
-            self.handlePanGestureEnd()
-            
-        default:
-            
-            break
-            
-        }
-        
-    }
-    
-    /** The absolute translation to make on the content view */
-    fileprivate func translateContentView(inXDimension x: CGFloat, animated: Bool = false) {
-
-        // Do not translate if the translation is at the maximum
-        guard abs(x) <= self.maxAbsoluteContentTranslation else {return}
-        
-        // Do not translate, if there is no menu
-        if x > 0 && self.leftMenuController == nil {
-            self.hideMenu(animated: false)
-            return
-        }
-        
-        if x < 0 && self.rightMenuController == nil {
-            self.hideMenu(animated: false)
-            return
-        }
-        
-        // Add overlay
-        self.addContentOverlay()
-        
-        let relativeTranslation = x / maxAbsoluteContentTranslation
-        
-        self.contentViewCenterConstraint.constant = x
-        
-        // 3d transforms
-        
-        let relative3dAngleTranslation: CGFloat = -(self.menuTilt * relativeTranslation)
-        let contentContainerViewWidthAfterTranslation = self.contentContainerView.bounds.size.width * cos(Angle.degreesToRadians(degrees: relative3dAngleTranslation))
-        var relative3dXTranslation: CGFloat =  self.contentContainerView.bounds.size.width - contentContainerViewWidthAfterTranslation
-        relative3dXTranslation = x > 0 ? -relative3dXTranslation : relative3dXTranslation
-        let relative3dScaleTranslation = 1 - (abs(relativeTranslation) * (1 - self.contentScaleFactor))
-        
-        var transform = CATransform3DIdentity;
-        transform = CATransform3DScale(transform, 1, relative3dScaleTranslation, 1)
-        transform.m34 = 1.0 / -1500;
-        transform = CATransform3DRotate(transform, Angle.degreesToRadians(degrees: relative3dAngleTranslation), 0, 1, 0.0);
-        transform = CATransform3DTranslate(transform, relative3dXTranslation, 0, 0)
-        
-        if animated {
-            
-            UIView.animate(withDuration: self.menuAnimationDuration, delay: 0, options: self.menuAnimationOptions, animations: {
-                
-                self.contentContainerView.layer.transform = transform;
-                self.view.layoutSubviews()
-                
-                }, completion: nil)
-            
-        } else {
-            
-            self.contentContainerView.layer.transform = transform;
-            
-        }
-
-    }
-    
-    fileprivate func handlePanGestureEnd() {
-        
-        if abs(self.contentViewCenterConstraint.constant) > self.maxAbsoluteContentTranslation / 2.0 {
-            
-            // Almost opened
-            
-            self.contentViewCenterConstraint.constant > 0 ? self.showMenu(.left) : self.showMenu(.right)
-            
-            
-        } else {
-            
-            self.hideMenu()
-            
-        }
-        
-    }
-    
-    fileprivate func showMenu(_ side: Side) {
-        
-        let translation = side == .left ? self.maxAbsoluteContentTranslation : -self.maxAbsoluteContentTranslation
-        
-        self.translateContentView(inXDimension: translation, animated: true)
-        
-    }
-    
-    fileprivate func toggleMenu(_ side: Side) {
-        
-        if self.isLeftMenuShown || self.isRightMenuShown {
-            
-            self.hideMenu()
-            
-        } else {
-            
-            self.showMenu(side)
-        }
+        self.animationDelegate.sideMenu(panGestureRecognized: recognizer)
         
     }
     
--- PGSideMenu/Classes/PGSideMenuSlideInRotateAnimator.swift
+++ PGSideMenu/Classes/PGSideMenuSlideInRotateAnimator.swift
@@ -28,13 +28,71 @@ class PGSideMenuSlideInRotateAnimator {
     
     required init(sideMenu: PGSideMenu) {
         self.sideMenu = sideMenu
+        
+        
+        self.configureSideMenu()
     }
     
     // MARK: Methods
     
+    func configureSideMenu() {
+        
+        self.sideMenu.leftMenuWidthConstraint.constant = self.maxAbsoluteContentTranslation
+        self.sideMenu.rightMenuWidthConstraint.constant = self.maxAbsoluteContentTranslation
+    }
+    
     func translateContentView(inXDimension x: CGFloat, animated: Bool) {
         
+        // Do not translate if the translation is at the maximum
+        guard abs(x) <= self.maxAbsoluteContentTranslation else {return}
+        
+        // Do not translate, if there is no menu
+        if x > 0 && self.sideMenu.leftMenuController == nil {
+            self.closeMenu(animated: false)
+            return
+        }
+        
+        if x < 0 && self.sideMenu.rightMenuController == nil {
+            self.closeMenu(animated: false)
+            return
+        }
+        
+        // Add overlay
+        self.sideMenu.addContentOverlay()
+        
+        let relativeTranslation = x / maxAbsoluteContentTranslation
+        
+        self.sideMenu.contentViewCenterConstraint.constant = x
+        
+        // 3d transforms
+        
+        let relative3dAngleTranslation: CGFloat = -(self.menuTilt * relativeTranslation)
+        let contentContainerViewWidthAfterTranslation = self.sideMenu.contentContainerView.bounds.size.width * cos(Angle.degreesToRadians(degrees: relative3dAngleTranslation))
+        var relative3dXTranslation: CGFloat =  self.sideMenu.contentContainerView.bounds.size.width - contentContainerViewWidthAfterTranslation
+        relative3dXTranslation = x > 0 ? -relative3dXTranslation : relative3dXTranslation
+        let relative3dScaleTranslation = 1 - (abs(relativeTranslation) * (1 - self.sideMenu.contentScaleFactor))
+        
+        var transform = CATransform3DIdentity;
+        transform = CATransform3DScale(transform, 1, relative3dScaleTranslation, 1)
+        transform.m34 = 1.0 / -1500;
+        transform = CATransform3DRotate(transform, Angle.degreesToRadians(degrees: relative3dAngleTranslation), 0, 1, 0.0);
+        transform = CATransform3DTranslate(transform, relative3dXTranslation, 0, 0)
+        
+        if animated {
             
+            UIView.animate(withDuration: self.sideMenu.menuAnimationDuration, delay: 0, options: self.sideMenu.menuAnimationOptions, animations: {
+                
+                self.sideMenu.contentContainerView.layer.transform = transform;
+                self.sideMenu.view.layoutSubviews()
+                
+                }, completion: nil)
+            
+        } else {
+            
+            self.sideMenu.contentContainerView.layer.transform = transform;
+            
+        }
+
     }
     
 }
@@ -58,12 +116,12 @@ extension PGSideMenuSlideInRotateAnimator: PGSideMenuAnimationDelegate {
     
     func toggleLeftMenu(animated: Bool = true) {
         
-        self.isLeftMenuOpen ? self.closeLeftMenu() : self.openLeftMenu()
+        self.toggleMenu(side: .left)
     }
     
     func openLeftMenu(animated: Bool = true) {
         
-        
+        self.translateContentView(inXDimension: self.maxAbsoluteContentTranslation, animated: animated)
     }
     
     func closeLeftMenu(animated: Bool = true) {
@@ -78,11 +136,12 @@ extension PGSideMenuSlideInRotateAnimator: PGSideMenuAnimationDelegate {
     
     func toggleRightMenu(animated: Bool = true) {
         
-        self.isRightMenuOpen ? self.closeRightMenu() : self.openRightMenu()
+        self.toggleMenu(side: .right)
     }
     
     func openRightMenu(animated: Bool = true) {
         
+        self.translateContentView(inXDimension: -self.maxAbsoluteContentTranslation, animated: animated)
     }
     
     func closeRightMenu(animated: Bool = true) {
@@ -97,11 +156,68 @@ extension PGSideMenuSlideInRotateAnimator: PGSideMenuAnimationDelegate {
     
     func closeMenu(animated: Bool = true) {
         
+        guard self.sideMenu.contentViewCenterConstraint.constant != 0 else {return}
         
+        self.sideMenu.contentOverlayView.removeFromSuperview()
+        
+        self.sideMenu.contentViewCenterConstraint.constant = 0
+        
+        if animated {
+            
+            UIView.animate(withDuration: self.sideMenu.menuAnimationDuration, delay: 0, options: self.sideMenu.menuAnimationOptions, animations: {
+                
+                self.sideMenu.contentContainerView.layer.transform = CATransform3DIdentity
+                self.sideMenu.view.layoutSubviews()
+                
+                }, completion: nil)
+            
+        } else {
+            
+            self.sideMenu.contentContainerView.layer.transform = CATransform3DIdentity
+            self.sideMenu.view.layoutSubviews()
+            
+        }
     }
     
-    func sideMenu(panGestureRecognized: UIPanGestureRecognizer) {
+    func sideMenu(panGestureRecognized recognizer: UIPanGestureRecognizer) {
         
+        let translation = recognizer.translation(in: self.sideMenu.view)
+        
+        switch recognizer.state {
+            
+        case .began:
+            
+            self.initialContentTranslation = self.sideMenu.contentViewCenterConstraint.constant
+            
+        case .changed:
+            
+            self.translateContentView(inXDimension: translation.x + self.initialContentTranslation, animated: false)
+            
+        case .ended:
+            
+            self.handlePanGestureEnd()
+            
+        default:
+            
+            break
+            
+        }
+    }
+    
+    func handlePanGestureEnd() {
+        
+        if abs(self.sideMenu.contentViewCenterConstraint.constant) > self.maxAbsoluteContentTranslation / 2.0 {
+            
+            // Almost opened
+            
+            self.sideMenu.contentViewCenterConstraint.constant > 0 ? self.openLeftMenu() : self.openRightMenu()
+            
+            
+        } else {
+            
+            self.closeMenu()
+            
+        }
         
     }
 }
